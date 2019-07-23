3003435d2807af57c4618370e37e945e1b45d086
Bryan <bryan@tortus.io>
2019-07-23 15:13:20 -0400

    fix syntax error

--- tortus/settings/prod.py
+++ tortus/settings/prod.py
@@ -7,7 +7,6 @@ INSTALLED_APPS += [
   'storages',
 ]
 
-# TODO: Generate new secret key and hide in environment variables on AWS
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # Postgres
@@ -40,4 +39,4 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'tortus.storage_backends.MediaStorage'
