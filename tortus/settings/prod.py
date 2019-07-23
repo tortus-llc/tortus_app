719873f8840c48123b32e504a37b6d363de5e0d1
Bryan <bryan@tortus.io>
2019-07-23 15:03:53 -0400

    setup for serving files with S3

--- tortus/settings/prod.py
+++ tortus/settings/prod.py
@@ -3,10 +3,14 @@ from .common import *
 # Override environment variables
 DEBUG = False
 
+INSTALLED_APPS += [
+  'storages',
+]
+
 # TODO: Generate new secret key and hide in environment variables on AWS
-SECRET_KEY = 'e85&&l8x!fd4(%5!86wp-pc!#exc3-axx!69qq=jlmm26_(wos'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-
+# Postgres
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -17,3 +21,23 @@ DATABASES = {
     'PORT': os.environ['RDS_PORT'],
   }
 }
+
+# Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'tortus/static'),
+]
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = 'tortus-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+  'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
